5fb6a55668e591c2395f4d8647bef3583871a285
Idg39 <idg39@txstate.edu>
2026-09-14 18:19:25 -0500

    Replaced the header with user prompt information and completed Lab 3

--- javascript/external.js
+++ javascript/external.js
@@ -4,8 +4,3 @@ const out = x + y;
 document.write(out);
 user_name = window.prompt("Please enter your name", "Type your name here");
 document.write(user_name);
-arr = ["Mael","Eric","Randall", "James"];
-for (var i=0; i < arr.length; i++)
-{
-  document.writeln(arr[i],"<br>");
-}
