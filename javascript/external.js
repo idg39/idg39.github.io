637f6190e37e2d265e095eeabee432460dea94bb
Idg39 <idg39@txstate.edu>
2026-09-14 17:36:29 -0500

    saved username

--- javascript/external.js
+++ javascript/external.js
@@ -1,22 +1,9 @@
-// document.write("Where is my JS code?");
-// window.alert(5 + 6);
-// window.alert('5 + 6');
-// document.write('<h1>A heading</h1>');
-// document.write('<p>A sentence.</p>');
-// const x = "web";
-// const y ="mapping";
-// const out = x + y;
-// document.write(out);
-// A prompt boc is used to prompt users to input a value before entering a page.
-// user_name = window.prompt("Please enter your name", "Type your name here");
-// document.write(user_name);
-// a = 22;
-// const b = 33;
-// document.writeln(a + b);
-// document.writeln("<br>");
-// document.writeln(a += b);
-// document.writeln("<br>");
-// document.writeln(a += b);
+const x = "web";
+const y ="mapping";
+const out = x + y;
+document.write(out);
+user_name = window.prompt("Please enter your name", "Type your name here");
+document.write(user_name);
 arr = ["Mael","Eric","Randall", "James"];
 for (var i=0; i < arr.length; i++)
 {
