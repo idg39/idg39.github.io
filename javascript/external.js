9cc7f74b6cff82c3738e97f646ee38edb4a60f31
Idg39 <idg39@txstate.edu>
2026-09-14 18:31:15 -0500

    Hotfix

--- javascript/external.js
+++ javascript/external.js
@@ -1,6 +1,3 @@
-const x = "web";
-const y ="mapping";
-const out = x + y;
 document.write(out);
 user_name = window.prompt("Please enter your name", "Type your name here");
 document.write(user_name);
