796a6d7d1f9335ede3ab7859e5e791401afccdac
Idg39 <idg39@txstate.edu>
2026-09-14 19:31:30 -0500

    Hotfix 2

--- javascript/external.js
+++ javascript/external.js
@@ -1,3 +1,2 @@
-document.write(out);
-user_name = window.prompt("Please enter your name", "Type your name here");
+var user_name = window.prompt("Please enter your name", "Type your name here");
 document.write(user_name);
