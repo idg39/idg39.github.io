16de515f183d1373ba43afaebd5ce0fdcde93142
Idg39 <idg39@txstate.edu>
2026-09-18 15:02:44 -0500

    Fix

--- javascript/external.js
+++ javascript/external.js
@@ -20,10 +20,7 @@ function webmap_table() {
   document.write("<table width=100%>");
   for (var row=0; row < webmaps.length; row++)
   {
-    if (row % 2 == 0) {
-      document.write("<tr>")
-    }
-      document.write("<tr>");
+    document.write("<tr>");
     for (var column=0; column < webmaps[0].length; column++)
     {
       document.write("<td>" + webmaps[row][column] + "</td>");
