0fafebbde5393c3fc4b77389a7722b97a62f4fde
Idg39 <idg39@txstate.edu>
2026-09-19 18:59:30 -0500

    test 4

--- javascript/external.js
+++ javascript/external.js
@@ -17,17 +17,16 @@ var webmaps =
 ];
 
 function webmap_table() {
-    document.write('<table width="100%">');
+    document.write('<table width="100%" cellpadding="5" cellspacing="0">');
     for (var row = 0; row < webmaps.length; row++) {
         if (row % 2 == 0) {
             document.write('<tr>');
-            document.write('<td>' + webmaps[row] + '</td>');
-            document.write('<td align="right">' + webmaps[row] + '</td>');
+            document.write('<td><strong>' + webmaps[row][0] + '</strong></td>');
+            document.write('<td align="right">' + webmaps[row][1] + '</td>');
             document.write('</tr>');
         } else {
             document.write('<tr>');
-            document.write('<td>' + webmaps[row] + '</td>');
-            document.write('<td align="right">' + webmaps[row] + '</td>');
+            document.write('<td colspan="2" style="padding-bottom: 15px;">' + webmaps[row][0] + '</td>');
             document.write('</tr>');
         }
     }
